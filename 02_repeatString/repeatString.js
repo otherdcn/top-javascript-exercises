301ed2bb262168d9e9892bb763569c9fa545192d
otherdcn <danny.c.ntwari@gmail.com>
2024-04-22 10:05:18 +0200

    Complete 02 Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
+  let loopCounter = 0;
+  let newString = '';
 
+  if (number >= 0) {
+  	while (loopCounter < number) {
+      newString += string;
+      loopCounter++;
+    }
+  } else {
+    return 'ERROR';
+  }
+  
+  return newString;
 };
 
 // Do not edit below this line
