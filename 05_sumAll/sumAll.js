6abbf975560f08444fe0777f59e8bd537104f19f
otherdcn <danny.c.ntwari@gmail.com>
2024-04-22 15:35:45 +0200

    Complete 05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum,lastNum) {
+  if ((firstNum < 0) || (lastNum < 0)) return 'ERROR';
+  if ((typeof firstNum != 'number') || (typeof lastNum != 'number')) return 'ERROR';
+  
+  let diff = 0;
+  let sum = 0;
+  
+	if (lastNum >= firstNum) {
+    diff = lastNum - firstNum; 
+    sum = firstNum;
 
+  	for (i = firstNum; i <= diff; i++) {
+    	sum += (firstNum+i);
+  	}
+  } else {
+    diff = firstNum - lastNum;
+    sum = lastNum;
+
+  	for (i = lastNum; i <= diff; i++) {
+    	sum += (lastNum+i);
+  	}
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
