2e6cd96a5b9e34a64a8cfafe6536b095ba38f345
otherdcn <danny.c.ntwari@gmail.com>
2024-04-26 22:09:46 +0200

    Complete 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return 	a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total,next) =>  total += next, 0);	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,next) => total * next)
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+  return base ** power;	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = n;
+
+  if (product === 0) return 1;
+  for (let i = 1; i < n; i++) product *= (n-i);
+  return product;
 };
 
 // Do not edit below this line
