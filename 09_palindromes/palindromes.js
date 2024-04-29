624d466df2ac8a4d230441c2ed7c254e08aef687
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 10:05:51 +0200

    Complete 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const unwantedChars = [" ",".",",","-",";",":","!","?"];
+  const arr = str.toLowerCase().split("");
 
+  const cleanArr = arr.filter((ele) => {
+    return !(unwantedChars.includes(ele));
+  });
+
+  const straightStr = cleanArr.join('');
+  const reverseStr = cleanArr.reverse().join('');
+
+  return straightStr === reverseStr;
 };
 
 // Do not edit below this line
