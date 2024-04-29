b4f5b6c4e96776b3d7957ba21c13709ffc2731ce
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 10:38:43 +0200

    Complete 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+  let a = 0
+	let b = 1
+	let sum = 0
+  let fiboSeq = [];
+  nth = Number(nth);
+	
+  if (nth === 0) return 0;
+  if (nth < 0) return "OOPS";
+  
+  for (let i = 0; i < nth; i++) {
+    sum = a + b;
+    a = b;
+    b = sum;
+    fiboSeq.push(a);
+  }
+  
+  console.log(fiboSeq);
+  
+  return fiboSeq[(nth-1)];
 };
 
 // Do not edit below this line
