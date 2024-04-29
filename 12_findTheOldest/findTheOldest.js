6e819247fa3364f5c6efec2ac02f83d932c9f0f7
otherdcn <danny.c.ntwari@gmail.com>
2024-04-29 11:50:03 +0200

    Complete 12 Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const sortedByOldest = array.sort((firstPerson,lastPerson) => {
+    const currentYear = (new Date()).getFullYear();
+    if (firstPerson.yearOfDeath === undefined) firstPerson.yearOfDeath = currentYear;
+    if (lastPerson.yearOfDeath === undefined) lastPerson.yearOfDeath = currentYear;
+    
+    firstPersonYears = (firstPerson.yearOfDeath - firstPerson.yearOfBirth);
+    lastPersonYears = (lastPerson.yearOfDeath - lastPerson.yearOfBirth);
+  
+    if (firstPersonYears > lastPersonYears) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  
+  return sortedByOldest[0];
 };
 
 // Do not edit below this line
